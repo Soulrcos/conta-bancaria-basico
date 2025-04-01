6d988dbf8a4a0957df743e60623a030d1e0c19c7
Marcos <marcos.pkinho@gmail.com>
2025-04-01 16:12:32 -0300

    Conta Bancaria v1.1 - Ajuste de redundancias

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
@@ -8,18 +7,17 @@ public class Main {
         double saldo = 0.00;
         Scanner leitor = new Scanner(System.in);
         int operacao = 0;
-        double transferencia = 0;
 
-        System.out.println(String.format(
+        System.out.printf(
                 """
-                ******************************************************
-                Dados iniciais do cliente:
-                
-                Nome:           %s
-                Tipo conta:     %s
-                Saldo inicial:  R$ %.2f
-                ******************************************************
-                """, nome, tipoConta, saldo));
+                        ******************************************************
+                        Dados iniciais do cliente:
+                        
+                        Nome:           %s
+                        Tipo conta:     %s
+                        Saldo inicial:  R$ %.2f
+                        ******************************************************
+                        %n""", nome, tipoConta, saldo);
 
         while (operacao != 4){
             System.out.println("""
@@ -36,16 +34,16 @@ public class Main {
 
             switch (operacao) {
                 case 1:
-                    System.out.println("O saldo atual é R$ %.2f".formatted(saldo));
+                    System.out.printf("O saldo atual é R$ %.2f%n", saldo);
                     break;
                 case 2:
                     System.out.println("Informe o valor a receber: ");
                     saldo += leitor.nextDouble();
-                    System.out.println("Saldo atualizado R$ %.2f".formatted(saldo));
+                    System.out.printf("Saldo atualizado R$ %.2f%n", saldo);
                     break;
                 case 3:
                     System.out.println("Informe o valor que deseja transferir:");
-                    transferencia = leitor.nextDouble();
+                    double transferencia = leitor.nextDouble();
                     if (transferencia > saldo){
                         System.out.println("Não há saldo suficiente para fazer essa tranferência.");
                     } else{
